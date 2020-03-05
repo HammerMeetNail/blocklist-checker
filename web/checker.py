92f8fca9549393db47d93145908954319a3b6d48
Dave O'Connor <1656866+HammerMeetNail@users.noreply.github.com>
2020-03-05 17:56:28 -0500

    Fixed url encoding and double slash issue

--- web/checker.py
+++ web/checker.py
@@ -4,25 +4,23 @@ from hashlib import sha256
 
 from boto3 import client
 from flask import Flask, jsonify, request
+from werkzeug.urls import iri_to_uri
 
 
 dynamodb_client = client('dynamodb')
 dynamodb_table = getenv("DYNAMODB_TABLE_NAME")
 
 app = Flask(__name__)
-
+app.url_map.merge_slashes = False
 
 @app.route("/urlinfo/1/<path:host>", defaults={'path': '/'})
 @app.route("/urlinfo/1/<path:host>/<path:path>")
 def check_url(host, path):
-    # Sanitize host and path. "/" should never be at the end of host, always be at the start of path
+
     if host.endswith("/"):
         host = host[:-1]
-    if not path.startswith("/"):
-        path = f"/{path}"
-    path = request.full_path[request.full_path.find(host) + len(host):]
-    if path.endswith("?"):
-        path = path[:-1]
+    host_offset = request.environ['RAW_URI'].find(host) + len(host)
+    path = request.environ['RAW_URI'][host_offset:]
 
     # Query DynamoDB for a single record - domain, sha256 and path must match exactly
     sha = sha256(f"{host}{path}".encode()).hexdigest()
